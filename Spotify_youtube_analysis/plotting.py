ca51a529ae660c7229577fa215e0f4398437f901
Swati Mishra <mishraswati1387@gmail.com>
2023-07-21 00:18:56 +0530

    update plotting.py, added deployment date to visualization

--- Spotify_youtube_analysis/plotting.py
+++ Spotify_youtube_analysis/plotting.py
@@ -93,7 +93,7 @@ Conclusion:
 ''')
 
 
-st.write("- By Swati Mishra", "\n", ""*2, "20-07-2023")
+st.write("- By Swati Mishra"+"\n  20-07-2023")
 
 #----End of Plotting.py file----------------------------------------------------------------
 # Coder: Swati Mishra
