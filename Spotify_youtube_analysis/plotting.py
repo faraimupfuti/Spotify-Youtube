172412a5f0e8f96c089467d80052da2e31852ccc
Swati Mishra <mishraswati1387@gmail.com>
2023-07-22 20:58:18 +0530

    updated analysis report

--- Spotify_youtube_analysis/plotting.py
+++ Spotify_youtube_analysis/plotting.py
@@ -15,12 +15,14 @@ st.subheader("Spotify_Youtube music and popularity data analysis using Matplotli
 st.write('''
 Analysis Report:
 1. Post malone and Ed sheeran are most listened artists and Ed sheeran is the most viewed artist for total 10 tracks.
-2. Bruno mars, Coldplay, Justin bieber and Ed sheeran have the least gap in their streams and views and have consistency
-   in their top 10 hits on Spotify and Youtube respectively.
-3. It's important to have good combination of valence, energy and liveness in one's tracks with significant accousticness
+2. Bruno mars, Coldplay, Justin bieber and Ed sheeran have the least gap in their streams and views showing consistent
+   appreciation for both videos and streams in their top 10 hits on Spotify and Youtube respectively.
+3. The views and streams analysed are based on total performance of top 10 tracks, and are not about the consistency.
+4. An artist can have extreme success on spotify streams, but very less on their youtube videos."
+5. It's important to have good combination of valence, energy and liveness in one's tracks with significant accousticness
    to achieve consistency in views and streams.
-4. However, having high energy, high loudness, and high danceability can give you very high streams for certain videos
-   compared to even consistent artists like in case of XXXTENTACION, but it can't ensure consistency in high views.
+6. However, having high energy, high loudness, and high danceability can give you very high streams for certain songs
+   compared to other artists like in case of XXXTENTACION, but it still can't ensure consistency in high views of the videos.
 ''')
 
 # file uploads
@@ -98,15 +100,15 @@ st.write(
 st.write('''
 Conclusion:
 1. Post malone and Ed sheeran are most listened artists and Ed sheeran is the most viewed artist for total 10 tracks.
-2. Bruno mars, Coldplay, Justin bieber and Ed sheeran have the least gap in their streams and views and have consistency
-   in their top 10 hits on Spotify and Youtube respectively.
-3. It's important to have good combination of valence, energy and liveness in one's tracks with significant accousticness
+2. Bruno mars, Coldplay, Justin bieber and Ed sheeran have the least gap in their streams and views showing consistent
+   appreciation for both videos and streams in their top 10 hits on Spotify and Youtube respectively.
+3. The views and streams analysed are based on total performance of top 10 tracks, and are not about the consistency.
+4. An artist can have extreme success on spotify streams, but very less on their youtube videos."
+5. It's important to have good combination of valence, energy and liveness in one's tracks with significant accousticness
    to achieve consistency in views and streams.
-4. However, having high energy, high loudness, and high danceability can give you very high streams for certain videos
-   compared to other artists, but it can't ensure consistency in high views.
+6. However, having high energy, high loudness, and high danceability can give you very high streams for certain songs
+   compared to other artists like in case of XXXTENTACION, but it can't ensure consistency in high views.
 ''')
-
-st.write("This isn't a proof of consistency but of total performance of top 10 tracks")
 
 st.write('''
 - Posted by - Swati Mishra
